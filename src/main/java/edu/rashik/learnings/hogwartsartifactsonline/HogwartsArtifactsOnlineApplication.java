9aa14e5c15c34382796e13b50c1626f13a64d323
rashikreddy <rashikreddy@gmail.com>
2024-08-26 07:21:55 -0500

    This is the first commit with no development

---
+++ src/main/java/edu/rashik/learnings/hogwartsartifactsonline/HogwartsArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.rashik.learnings.hogwartsartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactsOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsArtifactsOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/rashik/learnings/hogwartsartifactsonline/HogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rashik.learnings.hogwartsartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
